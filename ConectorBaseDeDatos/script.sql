71723fda0f7f98a398633dc3c0be767e4ba47ca5
July Alarcon <jurik17@hotmail.com>
2022-09-12 05:50:56 -0500

    Script base de datos inicial

---
+++ ConectorBaseDeDatos/script.sql
@@ -0,0 +1,15 @@
+create database if not exists Protalento01;
+use Protalento01;
+
+create table usuarios(
+	correo varchar(100) not null primary key,
+	clave blob, 
+	fechaCreacion date not null
+);
+
+create table if not exists categorias(
+	id bigint auto_increment primary key,
+	descripcion varchar(100)
+);
+
+
